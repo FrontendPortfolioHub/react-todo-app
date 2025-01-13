54d7741c2f9530ce10ed8f77b9feebf7317a8f6f
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-01-13 18:04:03 +0100

    implement mobile version

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
+import { Root } from './Root';
+
+const container = document.getElementById('root') as HTMLDivElement;
+
+createRoot(container)
+  .render(<Root />);
+
